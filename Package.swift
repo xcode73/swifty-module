5f7e143dba58b6332b7ad0b0418ecfa888761d42
Tibor Bödecs <mail.tib@gmail.com>
2020-08-23 22:57:29 +0200

    initial

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "swifty-module",
+    platforms: [
+       .macOS(.v10_15)
+    ],
+    products: [
+        .library(name: "SwiftyModule", targets: ["SwiftyModule"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/binarybirds/feather-core", from: "1.0.0"),
+        .package(url: "https://github.com/JohnSundell/Splash", from: "0.13.0"),
+    ],
+    targets: [
+        .target(name: "SwiftyModule",
+                dependencies: [
+                    .product(name: "FeatherCore", package: "feather-core"),
+                    .product(name: "Splash", package: "Splash"),
+                ]
+        ),
+        .testTarget(name: "SwiftyModuleTests",
+                    dependencies: [
+                        .target(name: "SwiftyModule"),
+                    ])
+    ]
+)
---
+++ Tests/SwiftyModuleTests/SwiftyModuleTests.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tibor Bodecs on 2020. 08. 23..
+//
+
+import XCTest
+@testable import SwiftyModule
+
+final class SwiftyModuleTests: XCTestCase {
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+    
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+}
