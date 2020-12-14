65387ad975c75490c5e8931c1ba3a70f148e08f7
Tibor Bödecs <mail.tib@gmail.com>
2020-12-14 21:29:32 +0100

    FC updates

--- Package.swift
+++ Package.swift
@@ -12,11 +12,22 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/binarybirds/feather-core", from: "1.0.0-beta"),
         .package(url: "https://github.com/JohnSundell/Splash", from: "0.15.0"),
+        
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
+        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.2.0-beta"),
     ],
     targets: [
         .target(name: "SwiftyModule", dependencies: [
             .product(name: "FeatherCore", package: "feather-core"),
             .product(name: "Splash", package: "Splash"),
+        ]),
+        .target(name: "Feather", dependencies: [
+            .product(name: "FeatherCore", package: "feather-core"),
+            
+            .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+            .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
+            
+            .target(name: "SwiftyModule"),
         ]),
         .testTarget(name: "SwiftyModuleTests", dependencies: [
             .target(name: "SwiftyModule"),
---
+++ Sources/Feather/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Feather
+//
+//  Created by Tibor Bodecs on 2019. 12. 17..
+//
+
+import FeatherCore
+import FluentSQLiteDriver
+import LiquidLocalDriver
+import SwiftyModule
+
+var env = try Environment.detect()
+try LoggingSystem.bootstrap(from: &env)
+let feather = try Feather(env: env)
+defer { feather.stop() }
+
+try feather.configure(database: .sqlite(.file("db.sqlite")),
+                      databaseId: .sqlite,
+                      fileStorage: .local(publicUrl: Application.baseUrl, publicPath: Application.Paths.public, workDirectory: "assets"),
+                      fileStorageId: .local,
+                      modules: [SwiftyBuilder()])
+try feather.start()
