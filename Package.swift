950912c69b2111575fbe1b5989621ba0040e24a6
xcode73 <xcode.uln@gmail.com>
2024-02-20 16:29:04 +0400

    .macOS(.v10_15),

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "swifty-module",
     platforms: [
-       .macOS(.v12)
+        .macOS(.v10_15),
     ],
     products: [
         .library(name: "SwiftyModule", targets: ["SwiftyModule"]),
