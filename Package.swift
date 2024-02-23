d0f8cc4ca4a135bd39515b17bcd1bbb8f2cb4036
xcode73 <xcode.uln@gmail.com>
2024-02-23 16:17:56 +0400

    swift-tools-version:5.9

--- Package.swift
+++ Package.swift
@@ -1,25 +1,26 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "swifty-module",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     products: [
-        .library(name: "SwiftyModule", targets: ["SwiftyModule"]),
+        .library(name: "SwiftyModule", targets: ["SwiftyModule"])
     ],
     dependencies: [
-        .package(url: "https://github.com/xcode73/feather-core", .branch("test-dev")),
-        .package(url: "https://github.com/JohnSundell/Splash", from: "0.16.0"),
+        .package(url: "https://github.com/xcode73/feather-core", branch: "test-dev"),
+        .package(url: "https://github.com/JohnSundell/Splash", from: "0.16.0")
     ],
     targets: [
         .target(name: "SwiftyModule", dependencies: [
             .product(name: "Feather", package: "feather-core"),
-            .product(name: "Splash", package: "Splash"),
+            .product(name: "Splash", package: "Splash")
         ],
         resources: [
-            .copy("Bundle"),
+            .copy("Bundle")
         ]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
